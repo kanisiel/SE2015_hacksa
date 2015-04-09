18200525570493c39391b0ecf3ef7d0d9e05ce57
kanisiel <linege3@nate.com>
2015-04-09 15:40:06 +0900

    first commit

---
+++ hacksa_web/src/main/java/control/MemberControl.java
@@ -0,0 +1,8 @@
+package control;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+public class MemberControl {
+
+}
---
+++ hacksa_web/src/main/java/model/CMemeber.java
@@ -0,0 +1,20 @@
+package model;
+
+public class CMemeber {
+	private String ID;
+	private String password;
+	
+	public String getID() {
+		return ID;
+	}
+	public void setID(String iD) {
+		ID = iD;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+		
+}
