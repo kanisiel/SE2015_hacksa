19fd8d242356d2cf98eba2eddf41240314fd34cd
Lee jaedeuk <linege3@nate.com>
2015-04-22 13:49:36 +0900

    remove unuse import

--- hacksa_web/src/main/java/kr/ac/mju/control/MemberControl.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.mju.control;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-public class MemberControl {
-
-}
